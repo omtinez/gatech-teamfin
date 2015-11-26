370ae1f1289ba5a47f3af5d2f1c86fa05987eab0
Addison Huddy <addisonhuddy@gmail.com>
2015-11-25 17:53:38 -0800

    sync with fhir method now ignores null values

--- helpers/jobs.py
+++ helpers/jobs.py
@@ -5,7 +5,25 @@ from FitbitAPI import FitbitAPI
 import sqlite3
 
 
-# schedule.every(60).minutes.do(sync_all_the_things)
+def sync_with_fhir():
+    for user in users:
+        print "-----------"
+        print user
+        fitbit_id = user[3]
+        user_id = user[0]
+        current_steps = user[4]
+        fitbit_stats = fbapi.pull(str(fitbit_id))
+        if fitbit_id is not None and user_id is not None:
+            if fitbit_stats['Steps'] > current_steps:
+                print "Steps different"
+                c.execute("UPDATE user SET current_steps=? WHERE id=?", (
+                    fitbit_stats['Steps'], int(user_id)))
+                diff_steps = fitbit_stats['Steps'] - current_steps
+                fhir.send_exercise_obs(diff_steps, user_id)
+                print "New steps saved"
+
+#TODO how do we start this on the server
+schedule.every(60).minutes.do(sync_with_fhir())
 fbapi = FitbitAPI()
 fhir = FHIR('http://polaris.i3l.gatech.edu:8080/gt-fhir-webapp/base/Observation')
 db = sqlite3.connect('../database/jogrx.db')
@@ -13,24 +31,5 @@ c = db.cursor()
 c.execute("SELECT * FROM user")
 users = c.fetchall()
 print users
-
-for user in users:
-    print "-----------"
-    print user
-    fitbit_id = user[3]
-    user_id = user[0]
-    current_steps = user[4]
-    fitbit_stats = fbapi.pull(str(fitbit_id))
-    if fitbit_stats['Steps'] > current_steps:
-        print "Steps different"
-        c.execute("UPDATE user SET current_steps=? WHERE id=?", (
-            fitbit_stats['Steps'], int(user_id)))
-        diff_steps = fitbit_stats['Steps'] - current_steps
-        fhir.send_exercise_obs(diff_steps, user_id)
-        print "New steps saved"
-
 db.commit()
 c.close()
-
-#  fbapi = FitbitAPI()
-# #     print fbapi.pull("3S2Y3M"
